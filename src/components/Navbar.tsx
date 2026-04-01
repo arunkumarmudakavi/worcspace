b6f0b45b4ec7cd402b48da84f70e39f8da30fd31
arunkumarmudakavi <arunkumarmudakavi029@gmail.com>
2026-04-01 17:49:05 +0100

    feat(Navbar): add header to navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Header from "./Header";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-[var(--primary-color)] rounded-2xl m-1">
+      <Header />
+    </nav>
+  );
+};
+
+export default Navbar;
